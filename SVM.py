6a3923b084b1eb9ce9d55bf2dd9e699772085f67
amar53 <acmar@uci.edu>
2016-03-10 20:23:53 -0800

    Added a basic SVM trainer on the data -very slow

--- SVM.py
+++ SVM.py
@@ -6,39 +6,44 @@ Created on Mar 5, 2016
 import numpy as np
 import matplotlib.pyplot as pl
 import mltools as ml
+from sklearn.svm import SVR
 
-X1=np.genfromtxt("data/kaggle.X1.train.txt", delimiter = ",")
-Y=np.genfromtxt("data/kaggle.Y.train.txt", delimiter = ",")
-Xtr,Xte,Ytr,Yte = ml.splitData(X1,Y,0.75)
 
-#Ihler's code for soft margin linear SVM - which might be useful to play around with
-#not prepared for the data
+#X1=np.genfromtxt("data/kaggle.X1.train.txt", delimiter = ",")
+#Y=np.genfromtxt("data/kaggle.Y.train.txt", delimiter = ",")
+#Xtr,Xte,Ytr,Yte = ml.splitData(X1,Y,0.75)
 
-class linearSVM(ml.classifier):
-    def predict(self, X):
-        Z = self.theta[:,0].T + X.dot( self.theta[:,1:].T )
-        Yhat = 2*(Z>0)-1  # np.sign(Z) without sign(0)=0
-        return Yhat
+#Trying to do this with Scikit learn
 
-sv = linearSVM()
+#WARNING: this takes a long time to run 
 
-from numpy import atleast_2d as twod
-from numpy import asarray as arr
+#Right now, the project is just training on every element in the training set
+X1 = np.genfromtxt("data/kaggle.X1.train.txt", delimiter=",")
+Y = np.genfromtxt("data/kaggle.Y.train.txt", delimiter=",")
 
-M,N = X.shape
-sv.theta = np.array([[-3,-1,.5]])
 
-alpha = 0.01
-reg = 1e-4
-for it in range(1000):   # 100 iterations:
-    for j in range(M):
-        zj = sv.theta[0,0]+twod(X[j,:]).dot(sv.theta[0,1:].T)  # compute linear response
-        #print zj
-        # Now, compute the gradient of the hinge loss:
-        gradj = 0 if zj*Y[j] > 1.0 else -Y[j]*arr([[1,X[j,0],X[j,1]]])
-        # plus the gradient of the L2 regularization term:
-        gradj += reg * sv.theta
-        # and update theta:
-        sv.theta -= alpha/(it+1) * gradj
-    if it<10: print "Error rate: {} \t(iter {})".format(sv.err(X,Y),it)
-print "...\nTheta:",sv.theta
+#testing the data
+Xe1 = np.genfromtxt("data/kaggle.X1.test.txt", delimiter=",")
+
+
+#There's three different kind of SVR according to sklearn: SVR, NuSVR, and LinearSVR
+#They are all based on libSVM.
+
+#In SVR, there are five built-in kernels: rbf, linear, poly, sigmoid, or precomputed.
+#Precomputed requires different parameters rather than just changing the parameter for kernel
+#With this default code below, which I ran with rbf, I got 0.83858, which is kinda bad.
+
+#We can also play around with C, gamma, and the other parameters in SVR
+
+svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+
+Ye = svr_rbf.fit(X1, Y).predict(Xe1)
+
+Ye = Ye.ravel()
+    
+fh = open('predictions.csv','w')    # open file for upload
+fh.write('ID,Prediction\n')         # output header line
+for i,yi in enumerate(Ye):
+  fh.write('{},{}\n'.format(i+1,yi)) # output each prediction
+fh.close()                          # close the file
+
