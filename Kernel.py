86a50fe8fcf1dd67bef2a9e22560e99f8fa54082
amar53 <acmar@uci.edu>
2016-03-11 21:36:19 -0800

    added MSE to SVM and a wip kernel predictor.

--- Kernel.py
+++ Kernel.py
@@ -1,4 +1,4 @@
-#placeholder for kernel SVM experiments
+#placeholder for kernel knn experiments
 '''
 Created on Mar 5, 2016
 
@@ -7,9 +7,28 @@ Created on Mar 5, 2016
 import numpy as np
 import matplotlib.pyplot as pl
 import mltools as ml
+from sklearn.neighbors.kde import KernelDensity
 
-X1=np.genfromtxt("data/kaggle.X1.train.txt", delimiter = ",")
-Y=np.genfromtxt("data/kaggle.Y.train.txt", delimiter = ",")
-Xtr,Xte,Ytr,Yte = ml.splitData(X1,Y,0.75)
+#X1=np.genfromtxt("data/kaggle.X1.train.txt", delimiter = ",")
+#Y=np.genfromtxt("data/kaggle.Y.train.txt", delimiter = ",")
+#Xtr,Xte,Ytr,Yte = ml.splitData(X1,Y,0.75)
 
-#Should we use the libsvm code from online?
+data = np.genfromtxt("data/curve80.txt", delimiter=None)
+X = data[:, 0]
+X = X[:, np.newaxis]
+Y = data[:, 1]
+Xtr, Xte, Ytr, Yte = ml.splitData(X, Y, 0.75)
+
+KernelLearner = KernelDensity(kernel='gaussian', bandwidth=0.2).fit(Xtr)
+Ye = KernelLearner.score_samples(Xtr)
+
+def KernelMSE(Y, Yhat):
+    error = 0
+    for i in range(len(Y)):
+        error += (Y[i] - Yhat[i])**2
+    return error / len(Y)
+
+currentMSE = KernelMSE(Ytr, Ye)
+
+print currentMSE
+
--- SVM.py
+++ SVM.py
@@ -4,27 +4,51 @@ Created on Mar 5, 2016
 @author: Ben
 '''
 import numpy as np
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 import mltools as ml
 from sklearn.svm import SVR
 
+#WARNING: this takes a long time to run with the kaggle training data
 
 #X1=np.genfromtxt("data/kaggle.X1.train.txt", delimiter = ",")
 #Y=np.genfromtxt("data/kaggle.Y.train.txt", delimiter = ",")
 #Xtr,Xte,Ytr,Yte = ml.splitData(X1,Y,0.75)
+#Kaggle testing data
+#Xe1 = np.genfromtxt("data/kaggle.X1.test.txt", delimiter=",")
 
-#Trying to do this with Scikit learn
-
-#WARNING: this takes a long time to run 
-
-#Right now, the project is just training on every element in the training set
-X1 = np.genfromtxt("data/kaggle.X1.train.txt", delimiter=",")
-Y = np.genfromtxt("data/kaggle.Y.train.txt", delimiter=",")
+data = np.genfromtxt("data/curve80.txt", delimiter=None)
+X = data[:, 0]
+X = X[:, np.newaxis]
+Y = data[:, 1]
+Xtr, Xte, Ytr, Yte = ml.splitData(X, Y, 0.75)
 
 
-#testing the data
-Xe1 = np.genfromtxt("data/kaggle.X1.test.txt", delimiter=",")
+#Training and predicting with the SVM
+svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+Ye = svr_rbf.fit(Xtr, Ytr).predict(Xte)
+#Ye = [4, 8.5, 15, 2, 10]
+#Yte = [5, 8, 10, 2, 11]
 
+def SVMMSE(Y, Yhat):
+    error = 0
+    for i in range(len(Y)):
+        error += (Y[i] - Yhat[i])**2
+    return error / len(Y)
+
+currentMSE = SVMMSE(Yte, Ye)
+
+print currentMSE
+
+#for writing the things to kaggle
+#svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+#Ye = svr_rbf.fit(X1, Y).predict(Xe1)
+#Ye = Ye.ravel()
+    
+#fh = open('predictions.csv','w')    # open file for upload
+#fh.write('ID,Prediction\n')         # output header line
+#for i,yi in enumerate(Ye):
+#  fh.write('{},{}\n'.format(i+1,yi)) # output each prediction
+#fh.close()                          # close the file
 
 #There's three different kind of SVR according to sklearn: SVR, NuSVR, and LinearSVR
 #They are all based on libSVM.
@@ -34,16 +58,3 @@ Xe1 = np.genfromtxt("data/kaggle.X1.test.txt", delimiter=",")
 #With this default code below, which I ran with rbf, I got 0.83858, which is kinda bad.
 
 #We can also play around with C, gamma, and the other parameters in SVR
-
-svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-
-Ye = svr_rbf.fit(X1, Y).predict(Xe1)
-
-Ye = Ye.ravel()
-    
-fh = open('predictions.csv','w')    # open file for upload
-fh.write('ID,Prediction\n')         # output header line
-for i,yi in enumerate(Ye):
-  fh.write('{},{}\n'.format(i+1,yi)) # output each prediction
-fh.close()                          # close the file
-
